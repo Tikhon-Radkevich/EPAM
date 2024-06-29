c19146102de9febc846a1f0cff3f557f1d3c324a
tikhon <tikhon.radk@gmail.com>
2024-06-29 15:13:18 +0300

    code formatting

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -20,7 +20,7 @@ from random import shuffle
 
 
 def n_random_permutations(speakers, n):
-    """ 3) Мат. Моделирование: N раз генерируем случайную перестановку """
+    """3) Мат. Моделирование: N раз генерируем случайную перестановку"""
     n_good_sequences = 0
     for _ in range(n):
         shuffle(speakers)
@@ -29,16 +29,22 @@ def n_random_permutations(speakers, n):
     return n_good_sequences / n
 
 
-def count_all_possible_options(speakers, curr_sequence, n_sequences=[0], n_good_sequences=[0]):
-    """ 2) Полный перебор всевозможных вариантов """
+def count_all_possible_options(
+    speakers, curr_sequence, n_sequences=[0], n_good_sequences=[0]
+):
+    """2) Полный перебор всевозможных вариантов"""
 
     if len(speakers) == 0:
         n_sequences[0] += 1
-        if curr_sequence.index("A") < curr_sequence.index("B") < curr_sequence.index("V"):
+        if (
+            curr_sequence.index("A")
+            < curr_sequence.index("B")
+            < curr_sequence.index("V")
+        ):
             n_good_sequences[0] += 1
 
     for i in range(len(speakers)):
-        remaining_speakers = speakers[:i] + speakers[i+1:]
+        remaining_speakers = speakers[:i] + speakers[i + 1:]
         new_sequence = curr_sequence + [speakers[i]]
         count_all_possible_options(remaining_speakers, new_sequence)
 
@@ -61,7 +67,7 @@ def main():
 
     # 3) Мат. Моделирование
     print("\n3) Мат. Моделирование:")
-    n = 5*10**5
+    n = 5 * 10**5
     prob = n_random_permutations(speakers, n)
     print(f"Количество случайных перестановок: {n}")
     print(f"Вероятность правильной расстановки: {prob}")
